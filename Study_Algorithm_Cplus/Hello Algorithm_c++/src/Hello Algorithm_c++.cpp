f129f5655815473f1c8c8e9b7ecd052ee5bdd310
shishaobo.good@163.com <tzjzbss@gmail.com>
2015-08-15 10:32:52 +0800

    algorithm use c++

---
+++ Study_Algorithm_Cplus/Hello Algorithm_c++/src/Hello Algorithm_c++.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : bss
+// Version     :
+// Copyright   : shishaobo.good@163.com
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
